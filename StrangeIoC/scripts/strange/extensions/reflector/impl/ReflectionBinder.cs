fdebb0ff461ef11f7abd4abcf7ab77986c00215d
unknown <tanakat@06-92Z.Anodius.local>
2015-07-20 20:22:15 +0200

    Modified for debugging.

--- StrangeIoC/scripts/strange/extensions/reflector/impl/ReflectionBinder.cs
+++ StrangeIoC/scripts/strange/extensions/reflector/impl/ReflectionBinder.cs
@@ -34,64 +34,64 @@ using System.Linq;
 
 namespace strange.extensions.reflector.impl
 {
-	public class ReflectionBinder : strange.framework.impl.Binder, IReflectionBinder
-	{
-		public ReflectionBinder ()
-		{
-		}
+    public class ReflectionBinder : strange.framework.impl.Binder, IReflectionBinder
+    {
+        public ReflectionBinder()
+        {
+        }
 
-		public IReflectedClass Get<T> ()
-		{
-			return Get (typeof(T));
-		}
+        public IReflectedClass Get<T>()
+        {
+            return Get(typeof(T));
+        }
 
-		public IReflectedClass Get (Type type)
-		{
-			IBinding binding = GetBinding(type);
-			IReflectedClass retv;
-			if (binding == null)
-			{
-				binding = GetRawBinding ();
-				IReflectedClass reflected = new ReflectedClass ();
-				mapPreferredConstructor (reflected, binding, type);
-				mapPostConstructors (reflected, binding, type);
-				mapSetters (reflected, binding, type);
-				binding.Bind (type).To (reflected);
-				retv = binding.value as IReflectedClass;
-				retv.PreGenerated = false;
-			}
-			else
-			{
-				retv = binding.value as IReflectedClass;
-				retv.PreGenerated = true;
-			}
-			return retv;
-		}
+        public IReflectedClass Get(Type type)
+        {
+            IBinding binding = GetBinding(type);
+            IReflectedClass retv;
+            if (binding == null)
+            {
+                binding = GetRawBinding();
+                IReflectedClass reflected = new ReflectedClass();
+                mapPreferredConstructor(reflected, binding, type);
+                mapPostConstructors(reflected, binding, type);
+                mapSetters(reflected, binding, type);
+                binding.Bind(type).To(reflected);
+                retv = binding.value as IReflectedClass;
+                retv.PreGenerated = false;
+            }
+            else
+            {
+                retv = binding.value as IReflectedClass;
+                retv.PreGenerated = true;
+            }
+            return retv;
+        }
 
-		public override IBinding GetRawBinding ()
-		{
-			IBinding binding = base.GetRawBinding ();
-			binding.valueConstraint = BindingConstraintType.ONE;
-			return binding;
-		}
+        public override IBinding GetRawBinding()
+        {
+            IBinding binding = base.GetRawBinding();
+            binding.valueConstraint = BindingConstraintType.ONE;
+            return binding;
+        }
 
-		private void mapPreferredConstructor(IReflectedClass reflected, IBinding binding, Type type)
-		{
-			ConstructorInfo constructor = findPreferredConstructor (type);
-			if (constructor == null)
-			{
-				throw new ReflectionException("The reflector requires concrete classes.\nType " + type + " has no constructor. Is it an interface?", ReflectionExceptionType.CANNOT_REFLECT_INTERFACE);
-			}
-			ParameterInfo[] parameters = constructor.GetParameters();
+        private void mapPreferredConstructor(IReflectedClass reflected, IBinding binding, Type type)
+        {
+            ConstructorInfo constructor = findPreferredConstructor(type);
+            if (constructor == null)
+            {
+                throw new ReflectionException("The reflector requires concrete classes.\nType " + type + " has no constructor. Is it an interface?", ReflectionExceptionType.CANNOT_REFLECT_INTERFACE);
+            }
+            ParameterInfo[] parameters = constructor.GetParameters();
 
 
-			Type[] paramList = new Type[parameters.Length];
-			object[] names = new object[parameters.Length];
-			int i = 0;
-			foreach (ParameterInfo param in parameters)
-			{
-				Type paramType = param.ParameterType;
-				paramList [i] = paramType;
+            Type[] paramList = new Type[parameters.Length];
+            object[] names = new object[parameters.Length];
+            int i = 0;
+            foreach (ParameterInfo param in parameters)
+            {
+                Type paramType = param.ParameterType;
+                paramList[i] = paramType;
 #if NETFX_CORE
 				object[] attributes = (object[])(param.GetCustomAttributes(typeof(Name), false));
                 //System.Diagnostics.Debug.WriteLine("attributes: "+attributes.ToString());
@@ -108,20 +108,20 @@ namespace strange.extensions.reflector.impl
                 }
                 i++;
             }
-			reflected.Constructor = constructor;
-			reflected.ConstructorParameters = paramList;
-			reflected.ConstructorParameterNames = names;
-		}
+            reflected.Constructor = constructor;
+            reflected.ConstructorParameters = paramList;
+            reflected.ConstructorParameterNames = names;
+        }
 
-		//Look for a constructor in the order:
-		//1. Only one (just return it, since it's our only option)
-		//2. Tagged with [Construct] tag
-		//3. The constructor with the fewest parameters
-		private ConstructorInfo findPreferredConstructor(Type type)
-		{
+        //Look for a constructor in the order:
+        //1. Only one (just return it, since it's our only option)
+        //2. Tagged with [Construct] tag
+        //3. The constructor with the fewest parameters
+        private ConstructorInfo findPreferredConstructor(Type type)
+        {
 #if NETFX_CORE
-            IEnumerable constructorsquery = type.GetTypeInfo().DeclaredConstructors.Where(m => m.IsPublic);
-            ConstructorInfo[] constructors = constructorsquery.Cast<ConstructorInfo>().ToArray();
+            IEnumerable<ConstructorInfo> constructors = type.GetTypeInfo().DeclaredConstructors;
+            //ConstructorInfo[] constructors = constructorsquery.Cast<ConstructorInfo>().ToArray();
             //ConstructorInfo[] constructors = (ConstructorInfo[])constructorsquery.ToArray();
             //System.Diagnostics.Debug.WriteLine("constructors: "+constructors.ToString());
             //System.Diagnostics.Debug.WriteLine("constructors: "+constructors.ToArray().GetValue(1).ToString());
@@ -132,21 +132,30 @@ namespace strange.extensions.reflector.impl
                                                                         BindingFlags.Instance |
                                                                         BindingFlags.InvokeMethod);
 
-#endif
-			if (constructors.Length == 1)
-			{
+            if (constructors.Length == 1)
+            {
                 System.Diagnostics.Debug.WriteLine("constructors: " + constructors[0].ToString());
-                return constructors [0];
+                return constructors[0];
+            }
+            int len;
+            int shortestLen = int.MaxValue;
+            ConstructorInfo shortestConstructor = null;
+            foreach (ConstructorInfo constructor in constructors)
+            {
+#endif
+#if NETFX_CORE
+            if (constructors.Count() == 1)
+			{
+                System.Diagnostics.Debug.WriteLine("constructors: " + constructors.FirstOrDefault());
+                return constructors.FirstOrDefault();
 			}
 			int len;
 			int shortestLen = int.MaxValue;
 			ConstructorInfo shortestConstructor = null;
-			foreach (ConstructorInfo constructor in constructors)
+            foreach (ConstructorInfo constructor in constructors.Where<ConstructorInfo>(y => !y.IsPublic))
 			{
-#if NETFX_CORE
+
                 object[] taggedConstructors = (Object[])(constructor.GetCustomAttributes(typeof(Construct), true));
-                // System.Diagnostics.Debug.WriteLine("taggedConstructors: "+taggedConstructors.ToString());
-                //System.Diagnostics.Debug.WriteLine("taggedConstructors: "+taggedConstructors.ToArray().GetValue(0).ToString());
 #else
                 object[] taggedConstructors = constructor.GetCustomAttributes(typeof(Construct), true);
 
@@ -155,23 +164,23 @@ namespace strange.extensions.reflector.impl
                 {
                     System.Diagnostics.Debug.WriteLine("constructor: " + constructor.ToString());
                     return constructor;
-				}
-				len = constructor.GetParameters ().Length;
-				if (len < shortestLen)
-				{
-					shortestLen = len;
-					shortestConstructor = constructor;
-				}
-			}
-			return shortestConstructor;
-		}
+                }
+                len = constructor.GetParameters().Length;
+                if (len < shortestLen)
+                {
+                    shortestLen = len;
+                    shortestConstructor = constructor;
+                }
+            }
+            return shortestConstructor;
+        }
 
-		private void mapPostConstructors(IReflectedClass reflected, IBinding binding, Type type)
-		{
+        private void mapPostConstructors(IReflectedClass reflected, IBinding binding, Type type)
+        {
 #if NETFX_CORE
-            IEnumerable<MethodInfo> methodsquery = type.GetTypeInfo().DeclaredMethods.Where(m => m.IsPublic);
+            IEnumerable<MethodInfo> methods = type.GetTypeInfo().DeclaredMethods.Where(m => m.IsPublic);
             //MethodInfo[] methods = methodsquery.Cast<MethodInfo>().ToArray();
-            MethodInfo[] methods = (MethodInfo[])methodsquery.ToArray();
+            //MethodInfo[] methods = (MethodInfo[])methodsquery.ToArray();
             //System.Diagnostics.Debug.WriteLine("methods: "+methodsquery.ToArray().GetValue(0).ToString());
 
 
@@ -181,9 +190,9 @@ namespace strange.extensions.reflector.impl
                                                          BindingFlags.Instance |
                                                          BindingFlags.InvokeMethod);
 #endif
-			ArrayList methodList = new ArrayList ();
-			foreach (MethodInfo method in methods)
-			{
+            ArrayList methodList = new ArrayList();
+            foreach (MethodInfo method in methods)
+            {
 #if NETFX_CORE
 
                 object[] tagged = (Object[])(method.GetCustomAttributes(typeof(PostConstruct), true));
@@ -196,14 +205,14 @@ namespace strange.extensions.reflector.impl
                 if (tagged.Length > 0)
                 {
                     System.Diagnostics.Debug.WriteLine("method: " + method.ToString());
-                    methodList.Add (method);
-				}
-			}
+                    methodList.Add(method);
+                }
+            }
 
-			methodList.Sort (new PriorityComparer ());
-			MethodInfo[] postConstructors = (MethodInfo[])methodList.ToArray (typeof(MethodInfo));
-			reflected.postConstructors = postConstructors;
-		}
+            methodList.Sort(new PriorityComparer());
+            MethodInfo[] postConstructors = (MethodInfo[])methodList.ToArray(typeof(MethodInfo));
+            reflected.postConstructors = postConstructors;
+        }
 
 #if NETFX_CORE
         bool HasPublicGetter(PropertyInfo pi)
@@ -216,14 +225,14 @@ namespace strange.extensions.reflector.impl
 #endif
 
         private void mapSetters(IReflectedClass reflected, IBinding binding, Type type)
-		{
-			KeyValuePair<Type, PropertyInfo>[] pairs = new KeyValuePair<Type, PropertyInfo>[0];
-			object[] names = new object[0];
+        {
+            KeyValuePair<Type, PropertyInfo>[] pairs = new KeyValuePair<Type, PropertyInfo>[0];
+            object[] names = new object[0];
 
 #if NETFX_CORE
-            IEnumerable<MemberInfo> privateMembersqyery = from m in type.GetTypeInfo().DeclaredMembers.OfType<MethodBase>() where !m.IsPublic select m;
+            IEnumerable<MemberInfo> privateMembers = from m in type.GetTypeInfo().DeclaredMembers.OfType<MethodBase>() where !m.IsPublic select m;
             //MemberInfo[] privateMembers = privateMembersqyery.Cast<MemberInfo>().ToArray();
-            MemberInfo[] privateMembers = (MemberInfo[])privateMembersqyery.ToArray();
+            //MemberInfo[] privateMembers = (MemberInfo[])privateMembersqyery.ToArray();
             //System.Diagnostics.Debug.WriteLine("privateMembers: "+privateMembers.ToString());
             //System.Diagnostics.Debug.WriteLine("privatemembers: "+privateMembers.ToArray().GetValue(0).ToString());
 #else
@@ -234,28 +243,33 @@ namespace strange.extensions.reflector.impl
                                                     BindingFlags.Instance,
                                                     null, null);
 #endif
-			foreach (MemberInfo member in privateMembers)
-			{
+            foreach (MemberInfo member in privateMembers)
+            {
 #if NETFX_CORE
                 object[] injections = (Object[])(member.GetCustomAttributes(typeof(Inject), true));
                 //System.Diagnostics.Debug.WriteLine("injections: "+injections.Count());
                 //System.Diagnostics.Debug.WriteLine("injections: "+injections.ToArray().GetValue(0).ToString());
+                 if (injections.Count() > 0)
+                {
+					throw new ReflectionException ("The class " + type.Name + " has a non-public Injection setter " + member.Name + ". Make the setter public to allow injection.", ReflectionExceptionType.CANNOT_INJECT_INTO_NONPUBLIC_SETTER);
+				}
 
 #else
                 object[] injections = member.GetCustomAttributes(typeof(Inject), true);
 
-
-#endif
                 if (injections.Length > 0)
                 {
-					throw new ReflectionException ("The class " + type.Name + " has a non-public Injection setter " + member.Name + ". Make the setter public to allow injection.", ReflectionExceptionType.CANNOT_INJECT_INTO_NONPUBLIC_SETTER);
-				}
-			}
+                    throw new ReflectionException("The class " + type.Name + " has a non-public Injection setter " + member.Name + ". Make the setter public to allow injection.", ReflectionExceptionType.CANNOT_INJECT_INTO_NONPUBLIC_SETTER);
+                }
+
+#endif
+
+            }
 
 #if NETFX_CORE
-            IEnumerable<MemberInfo> membersquery = from m in type.GetTypeInfo().DeclaredMembers.OfType<MethodBase>() where m.IsPublic select m;
+            IEnumerable<MemberInfo> members = type.GetTypeInfo().DeclaredMembers;
             //MemberInfo[] members = membersquery.Cast<MemberInfo>().ToArray();
-            MemberInfo[] members = (MemberInfo[])membersquery.ToArray();
+            //MemberInfo[] members = (MemberInfo[])membersquery.ToArray();
            // System.Diagnostics.Debug.WriteLine("members: "+members.ToString());
             //System.Diagnostics.Debug.WriteLine("members: "+members.ToArray().GetValue(0).ToString());
 
@@ -266,12 +280,15 @@ namespace strange.extensions.reflector.impl
                                                           BindingFlags.Public |
                                                           BindingFlags.Instance,
                                                           null, null);
+            foreach (MemberInfo member in members)
+            {
+                //System.Diagnostics.Debug.WriteLine("member: " + member);
 #endif
 
-			foreach (MemberInfo member in members)
-			{
-                //System.Diagnostics.Debug.WriteLine("member: " + member);
+
 #if NETFX_CORE
+             foreach (MemberInfo member in members.OfType<MethodBase>().Where<MethodBase>(m => !m.IsPublic))
+             {
                 System.Diagnostics.Debug.WriteLine("InjectMember: " + member.GetCustomAttributes(typeof(Inject), true));
                 object[] injections = (Object[])(member.GetCustomAttributes(typeof(Inject), true));
                 //System.Diagnostics.Debug.WriteLine("injections: "+injections.ToString());
@@ -279,55 +296,56 @@ namespace strange.extensions.reflector.impl
 
 #else
                 object[] injections = member.GetCustomAttributes(typeof(Inject), true);
-
-
+                
 #endif
-                if (injections.Length > 0)
+				if (injections.Length > 0)
                 {
-					Inject attr = injections [0] as Inject;
-                    System.Diagnostics.Debug.WriteLine("injections: " + attr);
-                    PropertyInfo point = member as PropertyInfo;
-					Type pointType = point.PropertyType;
-					KeyValuePair<Type, PropertyInfo> pair = new KeyValuePair<Type, PropertyInfo> (pointType, point);
-					pairs = AddKV (pair, pairs);
 
-					object bindingName = attr.name;
-					names = Add (bindingName, names);
-				}
-			}
-			reflected.Setters = pairs;
-			reflected.SetterNames = names;
-		}
+                        Inject attr = injections[0] as Inject;
+                        System.Diagnostics.Debug.WriteLine("injections: " + attr);
+                        PropertyInfo point = member as PropertyInfo;
+                        Type pointType = point.PropertyType;
+                        KeyValuePair<Type, PropertyInfo> pair = new KeyValuePair<Type, PropertyInfo>(pointType, point);
+                        pairs = AddKV(pair, pairs);
 
-		/**
+                        object bindingName = attr.name;
+                        names = Add(bindingName, names);
+                 }
+                reflected.Setters = pairs;
+                reflected.SetterNames = names;
+            }
+        }
+
+        /**
 		 * Add an item to a list
 		 */
-		private object[] Add(object value, object[] list)
-		{
-			object[] tempList = list;
-			int len = tempList.Length;
-			list = new object[len + 1];
-			tempList.CopyTo (list, 0);
-			list [len] = value;
-			return list;
-		}
+        private object[] Add(object value, object[] list)
+        {
+            object[] tempList = list;
+            int len = tempList.Length;
+            list = new object[len + 1];
+            tempList.CopyTo(list, 0);
+            list[len] = value;
+            return list;
+        }
 
-		/**
+        /**
 		 * Add an item to a list
 		 */
-		private  KeyValuePair<Type,PropertyInfo>[] AddKV(KeyValuePair<Type,PropertyInfo> value, KeyValuePair<Type,PropertyInfo>[] list)
-		{
-			KeyValuePair<Type,PropertyInfo>[] tempList = list;
-			int len = tempList.Length;
-			list = new KeyValuePair<Type,PropertyInfo>[len + 1];
-			tempList.CopyTo (list, 0);
-			list [len] = value;
-			return list;
-		}
-	}
+        private KeyValuePair<Type, PropertyInfo>[] AddKV(KeyValuePair<Type, PropertyInfo> value, KeyValuePair<Type, PropertyInfo>[] list)
+        {
+            KeyValuePair<Type, PropertyInfo>[] tempList = list;
+            int len = tempList.Length;
+            list = new KeyValuePair<Type, PropertyInfo>[len + 1];
+            tempList.CopyTo(list, 0);
+            list[len] = value;
+            return list;
+        }
+    }
 }
-
-	class PriorityComparer : IComparer
+namespace strange.extensions.reflector.impl
+{
+    class PriorityComparer : IComparer
 	{
 		int IComparer.Compare( Object x, Object y )
 		{
@@ -352,6 +370,7 @@ namespace strange.extensions.reflector.impl
 		}
 
 	}
+}
 
 
 
